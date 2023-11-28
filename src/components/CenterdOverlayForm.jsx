83c5eff07091ff0e86975ade53a438ba55dcfa6d
peachez-z <rkfld0911@gmail.com>
2023-11-28 15:05:46 +0900

    Design : 그룹 생성 컴포넌트 구현

--- src/components/CenterdOverlayForm.jsx
+++ src/components/CenterdOverlayForm.jsx
@@ -1,3 +1,29 @@
-export default function CenterdOverlayForm() {
-  return <div>CenterdOverlayForm</div>;
+import { Children } from "react";
+import { Container } from "react-bootstrap";
+import styled from "styled-components";
+import OverlayWrapper from "./shared/OverlayWrapper";
+
+export default function CenterdOverlayForm({ children }) {
+  return (
+    <StyledCentralizeContainer>
+      <StyledHeader>Dutch Pay</StyledHeader>
+      <OverlayWrapper>{children}</OverlayWrapper>
+    </StyledCentralizeContainer>
+  );
 }
+
+const StyledHeader = styled.h1`
+  font-weight: 200;
+  letter-spacing: 5px;
+`;
+
+const StyledCentralizeContainer = styled(Container)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 8px;
+  gap: 10px;
+  width: 50vw;
+  min-height: 100vh;
+`;
---
+++ src/components/shared/OverlayWrapper.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export default function OverlayWrapper({ children }) {
+  return <StyledContainer>{children}</StyledContainer>;
+}
+const StyledContainer = styled.div`
+  padding: ${(props) => props.padding || "5vw"};
+  background-color: white;
+  filter: drop-shadow(8px 4px 4px rgba(0, 0, 0, 0.25));
+  border-radius: 15px;
+  min-height: ${(props) => props.minheight || "0"};
+`;
